cd7f2e071ed5db9b049bcd148e8c6c1904fb1aff
Dulces Tentaciones Sin Tacc <96395905+DulcesTentacionessintacc@users.noreply.github.com>
2021-12-30 16:52:06 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,121 @@
+document.write('<select name="cremas"><option value="">-- ELIJA UN SABOR --</option></select>')
+
+function cambia_cremas(listindex)
+{
+
+document.tortas.cremas.options.length = 0;
+switch (listindex)
+{
+
+case "DULCE DE LECHE REPOSTERO" :
+document.tortas.cremas.options[0]=new Option("-- ELIJA UN SABOR --","-- ELIJA UN SABOR --");
+document.tortas.cremas.options[1]=new Option("CREMA PASTELERA","CREMA PASTELERA");
+document.tortas.cremas.options[2]=new Option("CREMA CHOCOLATE BLANCO","CREMA CHOCOLATE BLANCO");
+document.tortas.cremas.options[3]=new Option("CHANTILLY","CHANTILLY");
+document.tortas.cremas.options[4]=new Option("CHANTILLY CON FRUTOS ROJOS","CHANTILLY CON FRUTOS ROJOS");
+document.tortas.cremas.options[5]=new Option("CHANTILLY MIX FRUTAL","CHANTILLY MIX FRUTAL");
+document.tortas.cremas.options[6]=new Option("CREMA MOCCA","CREMA MOCCA");
+document.tortas.cremas.options[7]=new Option("GANACHE DE CHOCOLATE NEGRO","GANACHE DE CHOCOLATE NEGRO");
+
+break;
+
+case "GANACHE DE CHOCOLATE NEGRO" :
+document.tortas.cremas.options[0]=new Option("-- ELIJA UN SABOR --","-- ELIJA UN SABOR --");
+document.tortas.cremas.options[1]=new Option("COMBINADO DE REPOSTERO CON CHANTILLY","COMBINADO DE REPOSTERO CON CHANTILLY");
+document.tortas.cremas.options[2]=new Option("CHANTILLY","CHANTILLY");
+document.tortas.cremas.options[3]=new Option("CHANTILLY CON FRUTOS ROJOS","CHANTILLY CON FRUTOS ROJOS");
+document.tortas.cremas.options[4]=new Option("CREMA MOCCA","CREMA MOCCA");
+document.tortas.cremas.options[5]=new Option("CREMA PASTELERA","CREMA PASTELERA");
+document.tortas.cremas.options[6]=new Option("CREMA CHOCOLATE BLANCO","CREMA CHOCOLATE BLANCO");
+  
+break;
+
+case "GANACHE BLANCO CON MARROC" :
+document.tortas.cremas.options[0]=new Option("-- ELIJA UN SABOR --","-- ELIJA UN SABOR --");
+document.tortas.cremas.options[1]=new Option("CHANTILLY","CHANTILLY");
+document.tortas.cremas.options[2]=new Option("CREMA MOCCA","CREMA MOCCA");
+document.tortas.cremas.options[3]=new Option("DULCE DE LECHE REPOSTERO","DULCE DE LECHE REPOSTERO");
+
+break;
+
+case "COMBINADO DE REPOSTERO CON GANACHE NEGRO" :
+document.tortas.cremas.options[0]=new Option("-- ELIJA UN SABOR --","-- ELIJA UN SABOR --");
+document.tortas.cremas.options[1]=new Option("CHANTILLY","CHANTILLY");
+document.tortas.cremas.options[2]=new Option("CHANTILLY CON FRUTOS ROJOS","CHANTILLY CON FRUTOS ROJOS");
+
+break;
+
+}
+return true;
+}
+
+ //RELLENOS CON BIZCOCHUELO DE VAINILLA
+
+ function precio(){
+   
+  let bizcochuelo = document.querySelector("#bizcochuelo").value;
+  let listacremas = document.querySelector("#listacremas").value;
+  let cremas = document.querySelector("#cremas").value; 
+  let decoracion = document.querySelector("#decoracion").value;
+  let tamaño = document.querySelector("#tamaño").value;
+  
+  
+if(bizcochuelo = "BIZCOCHUELO DE VAINILLA" && listacremas == "DULCE DE LECHE REPOSTERO" && cremas == "CREMA PASTELERA" && decoracion == "CREMA DE LECHE" && tamaño == "10 PORCIONES"){
+  document.getElementById("salida").innerHTML = "$2000";
+}
+  else if (bizcochuelo = "BIZCOCHUELO DE VAINILLA" && listacremas == "DULCE DE LECHE REPOSTERO" && cremas == "CREMA CHOCOLATE BLANCO" && decoracion == "CREMA DE LECHE" && tamaño == "10 PORCIONES"){
+   document.getElementById("salida").innerHTML = "$2500";
+}
+else{}
+}
+
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+    
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  let telefono = "5493816069735";
+
+  let cliente = document.querySelector("#cliente").value;
+  let bizcochuelo = document.querySelector("#bizcochuelo").value;
+  let listacremas = document.querySelector("#listacremas").value;
+  let cremas = document.querySelector("#cremas").value;
+  let decoracion = document.querySelector("#decoracion").value;
+  let tamaño = document.querySelector("#tamaño").value;
+  let detalle = document.querySelector("#detalle").value;
+  let salida = document.querySelector("#salida").innerHTML;
+  let resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		--DULCES TENTACIONES SIN TACC--%0A
+		*PEDIDO*%0A%0A
+		*¿Cuál es tu nombre?*%0A
+		${cliente}%0A%0A
+		*Indica el tipo de bizcochuelo*%0A
+		${bizcochuelo}%0A%0A
+		*Indica el relleno N°1*%0A
+		${listacremas}%0A%0A
+		*Indica el relleno N°2*%0A
+		${cremas}%0A%0A
+    *¿Cuál es la decoración?*%0A
+		${decoracion}%0A%0A
+	  *¿Cuál es el tamaño de la torta?*%0A
+		${tamaño}%0A%0A
+    *Descripcion de la torta*%0A
+		${detalle}%0A%0A
+    *Precio*%0A
+		${salida}%0A%0A`;
+
+  if (cliente === "" || listacremas === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+
+  window.open(url);
+});
